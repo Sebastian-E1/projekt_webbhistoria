27a6ef2095ce6daea979c5767a3d017e69a9c139
Sebastian-E1 <54535891+Sebastian-E1@users.noreply.github.com>
2020-01-27 15:22:19 +0100

    Javascript
    La till javascript i hur hemsidor har utvecklats.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("hejsan");
+var is2005=true;
+
+function platsbyte(){
+    if(is2005 ){
+        document.getElementById("platsbyte").src = "/dragonslair2020.jpg";
+is2005=false;
+    } else{
+        document.getElementById("platsbyte").src = "/dragonslair2005.png";
+        is2005=true
+
+    }
+       
+}
+
+document.getElementById("platsbyte").onclick = platsbyte;
+
